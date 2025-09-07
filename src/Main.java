2ecf3c3b6ab5661d3d2d3ac7dc14b3ac01c8e882
Shahla Meem <meemst@farmingdale.edu>
2025-09-07 03:03:27 -0400

    HI from Shahla

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void Himain(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.printf("Hello ilknur!");
+        System.out.println("Hello ilknur!");
 
 
     }
